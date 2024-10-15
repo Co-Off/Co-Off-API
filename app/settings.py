884c3021d03048486d5c10711c00ec3f62451710
luisc5martins <luisc.martinsjunior5@gmail.com>
2024-10-15 16:41:37 -0300

    Adição de novos produtos

--- app/settings.py
+++ app/settings.py
@@ -151,7 +151,7 @@ REST_FRAMEWORK = {
     # "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly",), # Permissões através dos grupos do Django
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
-    "PAGE_SIZE": 10,
+    "PAGE_SIZE": 100,
     #"DEFAULT_AUTHENTICATION_CLASSES": ("rest_framework_simplejwt.authentication.JWTAuthentication",), # Autenticação com JWT
 }
 
