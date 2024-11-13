20c4786c3bc47d1109399d21b357df1fb95bfc86
luisc5matins <luisc.matinsjunior5@gmail.com>
2024-11-13 16:06:58 -0300

    Filtrando compras do usuário autenticado

--- uploader/views.py
+++ uploader/views.py
@@ -2,6 +2,8 @@ from rest_framework import mixins, parsers, viewsets
 
 from uploader.models import Document, Image
 from uploader.serializers import DocumentUploadSerializer, ImageUploadSerializer
+from core.models import Compra
+from rest_framework.viewsets import ModelViewSet
 
 
 class CreateViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, viewsets.GenericViewSet):
@@ -18,3 +20,14 @@ class ImageUploadViewSet(CreateViewSet):
     queryset = Image.objects.all() #  pylint: disable=no-member
     serializer_class = ImageUploadSerializer
     parser_classes = [parsers.FormParser, parsers.MultiPartParser]
+
+class CompraViewSet(ModelViewSet):
+    queryset = Compra.objects.all()
+
+    def get_queryset(self):
+        usuario = self.request.user
+        if usuario.is_superuser:
+            return Compra.objects.all()
+        if usuario.groups.filter(name="Administradores"):
+            return Compra.objects.all()
+        return Compra.objects.filter(usuario=usuario)
